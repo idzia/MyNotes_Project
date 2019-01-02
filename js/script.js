7d293c27b821d292dc757bcc118fe0db944bec90
idzia <ania.idzi@gmail.com>
2019-01-02 09:35:19 +0100

    Change declare variable

--- js/script.js
+++ js/script.js
@@ -3,91 +3,91 @@ function Note (title, content) {
     this.content = content
 };
 
-var preparNoteHTML = function (note) {
+let preparNoteHTML = function (note) {
     (!note.title) ? title = "" : title = note.title;
     (note.content == undefined) ? content = "" : content = note.content;
 
-    return '<div class="note">' +
-    '<button class="remove"></button>' +
+    return '<button class="remove"></button>' +
     '<input type="text" value="' + title + '" class="title" placeholder="Note Title"/>' + 
-    '<textarea rows="10" cols="25" class="content" placeholder="Note Content" >' + content +' </textarea>' +
-    '</div>';
-    //'<input type="text"  value="' + content + '" class="content" placeholder="Note Content" />' + 
+    '<textarea rows="10" cols="25" class="content" placeholder="Note Content" >' + content + '</textarea>';
+    
 };
 
-var putNoteInToHTML = function (note) {
+let putNoteInToHTML = function (note) {
+
     if (!note) {
         note = new Note("", "");
     }
-        
-    var noteHTML = preparNoteHTML(note);
-    document.getElementById("table").innerHTML += noteHTML;
-    
-    var buttonsDeleteArray = document.querySelectorAll('.remove');
+
+    let div = document.createElement('div');
+    div.setAttribute('class', 'note');
+    div.innerHTML = preparNoteHTML(note);
+    document.getElementById("table").appendChild(div);
+
+    let buttonsDeleteArray = document.querySelectorAll('.remove');
    
-    for (var i=0; i < buttonsDeleteArray.length; i++) {
+    for (let i=0; i < buttonsDeleteArray.length; i++) {
 
         buttonsDeleteArray[i].addEventListener('click', deleteNote);
     }
 
-    var nodeNoteList = document.querySelectorAll("div.note"); 
+    let nodeNoteList = document.querySelectorAll("div.note"); 
     
-    for (var i=0; i < nodeNoteList.length; i++) {
+    for (let i=0; i < nodeNoteList.length; i++) {
     
-        var titleNote = nodeNoteList[i].querySelector("input.title");
-        var contentNote = nodeNoteList[i].querySelector("textarea.content");
+        let titleNote = nodeNoteList[i].querySelector("input.title");
+        let contentNote = nodeNoteList[i].querySelector("textarea.content");
         
         titleNote.addEventListener("change", updateLocalStorage);
         contentNote.addEventListener("change", updateLocalStorage);
     };
 };
 
-var deleteNote = function(event) {
+let deleteNote = function(event) {
     
-    var child = event.target.parentNode;
-    var parent = child.parentNode;
+    let child = event.target.parentNode;
+    let parent = child.parentNode;
     parent.removeChild(child);
     updateLocalStorage();
 };
 
-var updateLocalStorage = function() {
+let updateLocalStorage = function() {
+    console.log('updateLocalStorage');
    
-    var nodeNoteList = document.querySelectorAll("div.note");  
-    var toJson = "";
-    var noteArray = [];
+    let nodeNoteList = document.querySelectorAll("div.note");  
+    let toJson = "";
+    let noteArray = [];
    
-    for (var i=0; i < nodeNoteList.length; i++) {
+    for (let i=0; i < nodeNoteList.length; i++) {
     
-        var nodeNote = nodeNoteList[i];
-        var titleNote = nodeNote.querySelector("input.title").value;
-        var contentNote = nodeNote.querySelector("textarea.content").value;
-        //var contentNote = nodeNote.querySelector("input.content").value;
+        let nodeNote = nodeNoteList[i];
+        let titleNote = nodeNote.querySelector("input.title").value;
+        let contentNote = nodeNote.querySelector("textarea.content").value;
+        //let contentNote = nodeNote.querySelector("input.content").value;
 
-        var note = new Note(titleNote, contentNote);
+        let note = new Note(titleNote, contentNote);
         noteArray.push(note);
     };
     localStorage.setItem("noteJSON", JSON.stringify(noteArray));
 };
 
-var readLocalStorage = function() {
-    var noteArray = [];
-    var htmlNotes = "";
-    var json = localStorage.getItem("noteJSON");
+let readLocalStorage = function() {
+    let noteArray = [];
+    let htmlNotes = "";
+    let json = localStorage.getItem("noteJSON");
     
     if (json != null) {
         noteArray = JSON.parse(json);
-    };
-    
-    for (var i=0; i < noteArray.length; i++) {
+    }
+
+    for (let i=0; i < noteArray.length; i++) {
         putNoteInToHTML(noteArray[i]);
-    };
+    }
 };
 
-var buttonAddNote = document.querySelector('#add');
+let buttonAddNote = document.querySelector('#add');
 buttonAddNote.addEventListener('click', putNoteInToHTML);
 
-// var buttonSave = document.querySelector('#save');
-// buttonSave.addEventListener('click', updateLocalStorage);
 
 readLocalStorage();
 
